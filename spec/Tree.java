415d43629484fafa4fb955539f8d374be768b15e
peterwy22 <524437285@qq.com>
2015-10-19 11:13:33 +1100

    completed tree

--- spec/Tree.java
+++ spec/Tree.java
@@ -12,8 +12,10 @@ public class Tree {
     private double[] myPos;
     private int stacks = 20;
     private int slices = 10;
-    private double radius = 0.6;
+    private double radius = 0.5;
+    private double trunkRadius = 0.1;
     private double height = 1;
+    private static final int NUM_OF_SLICES = 32;
     public Tree(double x, double y, double z) {
         myPos = new double[3];
         myPos[0] = x;
@@ -69,8 +71,34 @@ public class Tree {
     	gl.glPopMatrix();
     }
     
-    public void drawTrunk(){
-    	
+    public void drawTrunk(GL2 gl){
+    	gl.glBegin(GL2.GL_QUADS);
+		{	
+        	double angle = 0;
+        	double angleIncrement = 2*Math.PI/NUM_OF_SLICES;
+        	for(int i=0; i <= NUM_OF_SLICES; i++){
+        		angle = i* angleIncrement;
+        		double x1 = trunkRadius * Math.cos(angle);
+        		double z1 = trunkRadius * Math.sin(angle);
+        		
+        		angle = (i+1)* angleIncrement;
+        		double x2 = trunkRadius * Math.cos(angle);
+        		double z2 = trunkRadius * Math.sin(angle);
+        
+        		double[] p1 = {x1,0,z1};
+        		double[] p2 = {x2,0,z2};
+        		double[] p3 = {x2,height,z2};
+        		double[] n = MathUtil.getNormal(p1, p2, p3);
+    			n = MathUtil.normalise(n);
+    			gl.glNormal3d(n[0],n[1],n[2]);
+        		gl.glVertex3d(x1, 0, z1);
+        		gl.glVertex3d(x2, 0, z2);
+        		gl.glVertex3d(x2, height, z2);
+        		gl.glVertex3d(x1, height, z1);
+        
+        	} 
+    	}
+    	gl.glEnd();
     }
     
     public void draw(GL2 gl){
@@ -78,6 +106,7 @@ public class Tree {
     	gl.glPushMatrix();
     	gl.glTranslated(myPos[0], myPos[1], myPos[2]);
     	drawLeaf(gl);
+    	drawTrunk(gl);
     	gl.glPopMatrix();
     }
     
