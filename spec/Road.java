34d7f28092e77e2d110a149d68a2b8ba63fd1f2c
peterwy22 <524437285@qq.com>
2015-10-21 19:49:34 +1100

    new Road

--- spec/Road.java
+++ spec/Road.java
@@ -14,10 +14,12 @@ public class Road {
 
     private List<Double> myPoints;
     private double myWidth;
-    private static final int NUM_OF_POINTS = 32;
+    private static final int NUM_OF_POINTS = 64;
+    private static final int NUM_OF_SLICES = 32;
     private int stacks = 20;
     private int slices = 10;
     private double pathStoneRadius = 0.1;
+    private double expendRatio = 2;
     
     /** 
      * Create a new road starting at the specified point
@@ -95,6 +97,8 @@ public class Road {
     }
     
     public void draw(GL2 gl, Terrain terrain){
+    	gl.glPushMatrix();
+    	gl.glTranslated(0, 0.05, 0);
     	for (int i = 0; i < size(); i++ ){
     		double[] prePoint = null;
     		for (int j = 0; j < NUM_OF_POINTS; j++){
@@ -104,23 +108,127 @@ public class Road {
     				double[] v = {prePoint[0]-p[0],prePoint[1]-p[1]};
     				double[] n = {-v[1],0,v[0]};
     				n = MathUtil.normalise(n);
-    				double[] left = {p[0]+n[0]*myWidth/2,p[1]+n[2]*myWidth/2};
-    				double[] right = {p[0]-n[0]*myWidth/2,p[1]-n[2]*myWidth/2};
-    				gl.glPushMatrix();
-    					gl.glTranslated(left[0], terrain.altitude(left[0],left[1]), left[1]);
-    					drawPathStone(gl);
-    				gl.glPopMatrix();
-    				gl.glPushMatrix();
-    					gl.glTranslated(right[0], terrain.altitude(right[0],right[1]), right[1]);
-    					drawPathStone(gl);
-    				gl.glPopMatrix();
+    				
+    				double[] middleRight = {p[0],terrain.altitude(p[0],p[1]),p[1]};
+					double[] middlePreRight = {prePoint[0],terrain.altitude(prePoint[0],prePoint[1]),prePoint[1]};
+					double[] middleLeft = {p[0],terrain.altitude(p[0],p[1]),p[1]};
+					double[] middlePreLeft = {prePoint[0],terrain.altitude(prePoint[0],prePoint[1]),prePoint[1]};
+    				for (int s = 0; s < NUM_OF_SLICES;s++){
+    					double ratio = (1.0 + s)/NUM_OF_SLICES;
+    					double[] left = {p[0]+n[0]*myWidth/2*ratio,p[1]+n[2]*myWidth/2*ratio};
+    					double[] right = {p[0]-n[0]*myWidth/2*ratio,p[1]-n[2]*myWidth/2*ratio};
+    					double[] leftPre = {prePoint[0]+n[0]*myWidth/2*ratio,prePoint[1]+n[2]*myWidth/2*ratio};
+    					double[] rightPre = {prePoint[0]-n[0]*myWidth/2*ratio,prePoint[1]-n[2]*myWidth/2*ratio};
+    				
+    					double[] leftF = {left[0],terrain.altitude(left[0],left[1]),left[1]};
+    					double[] rightF = {right[0],terrain.altitude(right[0],right[1]),right[1]};
+    					double[] leftPreF = {leftPre[0],terrain.altitude(leftPre[0],leftPre[1]),leftPre[1]};
+    					double[] rightPreF = {rightPre[0],terrain.altitude(rightPre[0],rightPre[1]),rightPre[1]};
+    					
+    					
+    					double[] mv = new double[2];
+    					mv[0] = p[0] - prePoint[0];
+    					mv[1] = p[1] - prePoint[1];
+    					//mv = MathUtil.normalise(mv);
+    					gl.glPushMatrix();
+    						//gl.glTranslated(left[0], terrain.altitude(left[0],left[1]), left[1]);
+    						//drawPathStone(gl);
+    						double[] mpl = new double[3];
+    						mpl[0] = middlePreLeft[0] - mv[0]*expendRatio;
+    						mpl[1] = middlePreLeft[1];
+    						mpl[2] = middlePreLeft[2] - mv[1]*expendRatio;
+    						
+    						double[] ml = new double[3];
+    						ml[0] = middleLeft[0] + mv[0]*expendRatio;
+    						ml[1] = middleLeft[1];
+    						ml[2] = middleLeft[2] + mv[1]*expendRatio;
+    						
+    						double[] lf = new double[3];
+    						lf[0] = leftF[0] + mv[0]*expendRatio;
+    						lf[1] = leftF[1];
+    						lf[2] = leftF[2] + mv[1]*expendRatio;
+    						
+    						double[] lpf = new double[3];
+    						lpf[0] = leftPreF[0] - mv[0]*expendRatio;
+    						lpf[1] = leftPreF[1];
+    						lpf[2] = leftPreF[2] - mv[1]*expendRatio;
+    						
+    						drawTriangle(mpl,ml,lf, null, gl);
+    						drawTriangle( lf,lpf ,mpl, null, gl);
+    					gl.glPopMatrix();
+    					gl.glPushMatrix();
+    						//gl.glTranslated(right[0], terrain.altitude(right[0],right[1]), right[1]);
+    						//drawPathStone(gl);
+    						double[] mpr = new double[3];
+    						mpr[0] = middlePreRight[0] - mv[0]*expendRatio;
+    						mpr[1] = middlePreRight[1];
+    						mpr[2] = middlePreRight[2] - mv[1]*expendRatio;
+    						
+    						double[] rpf = new double[3];
+    						rpf[0] = rightPreF[0] - mv[0]*expendRatio;
+    						rpf[1] = rightPreF[1];
+    						rpf[2] = rightPreF[2] - mv[1]*expendRatio;
+    					
+    						drawTriangle(middlePreRight,rightPreF ,rightF, null, gl);
+    						drawTriangle(rightF,middleRight, middlePreRight, null, gl);
+    					gl.glPopMatrix();
+    					
+    					
+    				
+    					middleRight[0] = rightF[0];
+    					middleRight[1] = rightF[1];
+    					middleRight[2] = rightF[2];
+    					
+    					middlePreRight[0] = rightPreF[0];
+    					middlePreRight[1] = rightPreF[1];
+    					middlePreRight[2] = rightPreF[2];
+    					
+    					middleLeft[0] = leftF[0];
+    					middleLeft[1] = leftF[1];
+    					middleLeft[2] = leftF[2];
+    					
+    					middlePreLeft[0] = leftPreF[0];
+    					middlePreLeft[1] = leftPreF[1];
+    					middlePreLeft[2] = leftPreF[2];
+    				}
     			}
 			prePoint = new double[2];
 			prePoint[0] = p[0];
 			prePoint[1] = p[1];
     		}
     	}
+    	gl.glPopMatrix();
     }
+    
+    private void drawTriangle(double[] p1, double[] p2, double[] p3,double[] colour, GL2 gl) {
+    	//if (colour == null){
+    		//return;
+    	//}
+    	//System.out.print(p1[0] + ","+p1[1]+","+p1[2]+"|");
+    	//System.out.print(p2[0] + ","+p2[1]+","+p2[2]+"|");
+    	//System.out.println(p3[0] + ","+p3[1]+","+p3[2]+"|");
+    	
+    	//gl.glBindTexture(GL2.GL_TEXTURE_2D, myTerrainTexture.getTextureId());
+		gl.glBegin(GL2.GL_TRIANGLES);
+		{	
+			//I add textures amoung every vertex
+			gl.glColor4d(1,1,1,1);
+			double[] n = MathUtil.getNormal(p1, p2, p3);
+			n = MathUtil.normalise(n);
+			//System.out.println(n[1]);
+			gl.glNormal3d(n[0],n[1],n[2]);
+			gl.glTexCoord2d(0.0, 0.0);
+            gl.glVertex3d(p1[0], p1[1], p1[2]);
+            gl.glTexCoord2d(1.0, 0.0);
+            gl.glVertex3d(p2[0], p2[1], p2[2]);
+            gl.glTexCoord2d(0.5, 1.0);
+            gl.glVertex3d(p3[0], p3[1], p3[2]);
+            
+            
+           
+        }
+        gl.glEnd();        		
+	}
     
     /**
      * Get a point on the spine. The parameter t may vary from 0 to size().
